ed9557ec4fb97d6b369f8119ba8fa904148cda6b
nob02120302-hue <nob02120302@gmail.com>
2026-07-09 18:51:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ function selectJob(jobName) {
     console.log("選択した職業: " + jobName);
     showScene('spirit-scene');
 }
+// script.js の末尾にこれを追加
+
+function selectSpirit(spiritName) {
+    console.log("選択した精霊: " + spiritName);
+    showScene('adventure-scene'); // 冒険開始画面へ
+}
